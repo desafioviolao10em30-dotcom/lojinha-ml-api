c3162d0826b02c65103ae0f75644a9b1141dca37
desafioviolao10em30-dotcom <desafioviolao10em30@gmail.com>
2026-03-08 12:56:38 -0300

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,34 +1,3 @@
-async function refreshAccessToken() {
-
-  const params = new URLSearchParams();
-
-  params.append("grant_type", "refresh_token");
-  params.append("client_id", process.env.ML_CLIENT_ID);
-  params.append("client_secret", process.env.ML_CLIENT_SECRET);
-  params.append("refresh_token", process.env.ML_REFRESH_TOKEN);
-
-  const response = await fetch(
-    "https://api.mercadolibre.com/oauth/token",
-    {
-      method: "POST",
-      headers: {
-        "accept": "application/json",
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      body: params
-    }
-  );
-
-  const data = await response.json();
-
-  if (!response.ok) {
-    throw new Error(data.error_description || "Erro ao renovar token");
-  }
-
-  return data.access_token;
-}
-
-
 export default async function handler(req, res) {
 
   const { ids } = req.query;
